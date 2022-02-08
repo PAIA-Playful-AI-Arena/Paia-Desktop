754d924405f062ea82eb640a95a407c121728906
You-Cheng Xiao <jason53415@gmail.com>
2022-02-09 06:44:00 +0800

    Maximize the window when opening the program

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ const openAboutWindow = require('about-window').default
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
@@ -64,8 +62,8 @@ function createWindow () {
     }
   ];
   
+  mainWindow.maximize();
   Menu.setApplicationMenu(Menu.buildFromTemplate(template));
-
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
   // mainWindow.setMenuBarVisibility(false)

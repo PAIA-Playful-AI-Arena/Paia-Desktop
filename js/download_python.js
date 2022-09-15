b1b1d1605406b6b14ee84f9c50b9408df787af51
You-Cheng Xiao <jason53415@gmail.com>
2022-09-15 16:35:50 +0800

    Download Python env after clearing target folder

--- js/download_python.js
+++ js/download_python.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const { argv } = require('process');
 const download = require('download-git-repo');
 const version = 'v1.0.2'
@@ -7,8 +8,13 @@ if (argv.length < 3 || (['win32', 'darwin', 'linux']).indexOf(argv[2]) == -1) {
   return;
 }
 
+var dest = 'python/dist/interpreter';
+if (fs.existsSync(dest)) {
+  fs.rmSync(dest, { recursive: true });
+}
+
 console.log(`Python for ${argv[2]} downloading...`);
-download(`direct:https://github.com/jason53415/PAIA-Desktop-Python/releases/download/${version}/interpreter-${argv[2]}.zip`, 'python/dist/interpreter', function (err) {
+download(`direct:https://github.com/jason53415/PAIA-Desktop-Python/releases/download/${version}/interpreter-${argv[2]}.zip`, dest, function (err) {
   if (err) {
     console.log(`error: ${err}`);
   } else {

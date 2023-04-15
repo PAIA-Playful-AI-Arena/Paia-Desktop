7ec1ee9401c82dbcece7594d5643b9b4f5150c5e
You-Cheng Xiao <jason53415@gmail.com>
2023-04-16 05:27:41 +0800

    Update Python environment

--- js/download_python.js
+++ js/download_python.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { argv } = require('process');
 const download = require('download-git-repo');
-const version = 'v1.0.3'
+const version = 'v1.0.4'
 
 if (argv.length < 3 || (['win32', 'darwin', 'linux']).indexOf(argv[2]) == -1) {
   console.log('Usage: node download-python.js [win32, darwin, linux]')

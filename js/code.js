88b7abc88ad29987d94187ff1120c1acc4f21227
You-Cheng Xiao <jason53415@gmail.com>
2024-03-19 18:22:33 +0800

    Fix project file detection

--- js/code.js
+++ js/code.js
@@ -1807,7 +1807,7 @@ Code.updateProjectFileList = function(parent='') {
   if (parent == '')
     $(`#project-file-list`).empty();
   window.fs.readdirSync(window.path.join(Code.PROJECT_PATH, parent), { recursive: true }).forEach(file => {
-    if (window.dir.isDirectory(file)) {
+    if (window.dir.isDirectory(window.path.join(Code.PROJECT_PATH, parent, file))) {
       if (file != "__pycache__")
         Code.updateProjectFileList(window.path.join(parent, file));
     } else {

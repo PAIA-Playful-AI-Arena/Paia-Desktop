b795366580fe1ea69f1f55e57f29bda7bfabcd28
You-Cheng Xiao <jason53415@gmail.com>
2024-07-13 22:57:15 +0800

    Add zip maker for automatic update of macOS

--- forge.config.js
+++ forge.config.js
@@ -61,6 +61,12 @@ module.exports = {
       ]
     },
     {
+      "name": "@electron-forge/maker-zip",
+      "platforms": [
+        "darwin"
+      ]
+    },
+    {
       "name": "@electron-forge/maker-deb",
       "config": {
         "options": {
